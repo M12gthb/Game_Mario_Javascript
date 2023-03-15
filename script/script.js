0f738cfb345ec4b9cab90f3198ccd292dd9fa21b
Matheus Barros Rodrigues <mat43214@gmail.com>
2023-03-15 18:52:09 -0300

    Jogo-Mario

--- script/script.js
+++ script/script.js
@@ -1,22 +1,34 @@
 let count = 0
 const tube = document.querySelector('.game_tube')
 const mario = document.querySelector('.game_Mario')
+const point = document.querySelector('.Point')
 
 document.addEventListener('keydown', ()=>{
-    const point = document.querySelector('.Point')
     mario.classList.add('jump')
     setTimeout(()=>{
-        mario.classList.remove('jump')
-        count++
         point.innerText = `Pontuação: ${count}`
+        mario.classList.remove('jump')
+        
     },700)
+    count++
 })
 
 const loop = setInterval(() => {
     const tubePosition = tube.offsetLeft;
-    if(tubePosition <= 125){
+    const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '')
+    const gameOver = document.querySelector('.gameOver')
+    const replay = document.querySelector('.gameOver__h1')
+    if(tubePosition <= 125 && tubePosition > 0 && marioPosition < 80){
         tube.style.animation = 'none'
         tube.style.left = `${tubePosition}px`
+    
+        mario.style.animation = 'none'
+        mario.style.bottom = `${marioPosition}px`
+        mario.src = "./assets/Over.png"
 
+        gameOver.showModal()
+        replay.addEventListener('click', () =>{
+            window.location.replace('./index.html')
+        })
     }
 }, 10)
